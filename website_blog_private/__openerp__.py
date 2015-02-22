a984d2bf17df90dff63b0e6693a63ecdb7ea4184
Administrator <admin@opensur.com>
2015-02-22 00:35:35 -0200

    First version

---
+++ website_blog_private/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013-Today OpenSur.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Blogs private',
+    'category': 'Website',
+    'website': 'https://www.opensur.com',
+    'summary': 'Blogs private access',
+    'version': '1.0',
+    'description': """
+OpenERP Blog Private
+====================
+Add feature to have private blogs, visible only for certain security groups
+        """,
+    'author': 'OpenSur',
+    'depends': ['website_blog'],
+    'data': [
+        'data/access_rules.xml',
+        'views/website_blog_private_views.xml'
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+}
